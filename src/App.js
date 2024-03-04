c1ce80190da222432339eb91e5898de2f41306c3
Estrellita <142866059+EstrellaAbarca97@users.noreply.github.com>
2024-03-03 21:48:08 -0600

    actualizacion-

--- src/App.js
+++ src/App.js
@@ -1,70 +1,51 @@
-// App.js
-
 import React, { useState } from 'react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import logo from './logo.svg';
-import Carrusel from './Carrusel'; 
+import { FaFacebook, FaTwitter, FaInstagram, FaHome } from 'react-icons/fa';
+import Carrusel from './Carrusel';
 import './App.css';
-import ubiIcon from './img/ubi.png'; // Importa el icono de ubicación
-
 function App() {
-  const [showLogin, setShowLogin] = useState(false);
-
-  const handleLoginClick = () => {
-    setShowLogin(true);
-  };
+  const [showPackages, setShowPackages] = useState(false);
+  const [showServices, setShowServices] = useState(false);
+  const [showMegaMovil, setShowMegaMovil] = useState(false);
 
   return (
     <div className="Main">
       <header className="Header">
-        <div className="Links">
-          <a href="#" className="Link">Soporte técnico</a>
-          <span className="Separator"> | </span>
-          <a href="#" className="Link">Bolsa de trabajo</a>
-          <span className="Separator"> | </span>
-          <a href="#" className="Link">Mi correo</a>
+        <div className="SocialIcons">
+          <FaFacebook size={24} />
+          <FaTwitter size={24} />
+          <FaInstagram size={24} />
         </div>
-        <div className="Location">
-          <img src={ubiIcon} alt="Ubicación" className="LocationIcon" />
-          <a href="#" className="Link">Tuxtla Gutiérrez</a>
-        </div>
-        <div className="Social">
-          <a href="#"><FaFacebook className="SocialIcon" /></a>
-          <a href="#"><FaTwitter className="SocialIcon" /></a>
-          <a href="#"><FaInstagram className="SocialIcon" /></a>
-        </div>
-        <div className="Phone">
-          <span className="PhoneText">Ventas: 7577-363853</span>
-        </div>
+        <div className="ContactInfo">Phone: 123-456-7890</div>
       </header>
-      <div className="Bar">
-        <div className="Logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        <div className="Menu">
-          <button>Home</button>
-          <button onClick={handleLoginClick}>Button 1</button>
-          <button>Button 2</button>
-          <button>Button 3</button>
+      <div className="GrayHeader">
+        <FaHome size={24} />
+        <img src="./img/mega.png" alt="Mega" />
+        <div className="Links">
+          <a href="#" onClick={() => setShowPackages(true)}>Paquetes</a>
+          <a href="#" onClick={() => setShowServices(true)}>Servicios</a>
+          <a href="#" onClick={() => setShowMegaMovil(true)}>Mega Móvil</a>
         </div>
       </div>
-      {showLogin && (
-        <div className="LoginSection">
-          {/* Contenido del inicio de sesión */}
+      {showPackages && (
+        <div className="LandscapeContent">
+          {/* Contenido de los paquetes */}
+        </div>
+      )}
+      {showServices && (
+        <div className="LandscapeContent">
+          {/* Contenido de los servicios */}
+        </div>
+      )}
+      {showMegaMovil && (
+        <div className="LandscapeContent">
+          {/* Contenido de Mega Móvil */}
         </div>
       )}
       <div className="CarruselSection">
         <Carrusel />
-      </div>
-      <div className="OptionsBar">
-        <button>Option 1</button>
-        <button>Option 2</button>
-        <button>Option 3</button>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
